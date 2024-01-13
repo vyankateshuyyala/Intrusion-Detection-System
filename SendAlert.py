89f6d05c26b5d32e46877e48f5bd9369ae793bd1
vyankatesh <125289064+vyankateshuyyala@users.noreply.github.com>
2024-01-13 22:08:52 +0530

    Update SendAlert.py

--- SendAlert.py
+++ SendAlert.py
@@ -4,9 +4,9 @@ auth_token = 'auth_token'
 client = Client(account_sid, auth_token)
 def sendSms():
     message = client.messages.create(
-    from_='+91000000000',
+    from_='+910000000000',
     body='Alert ',
-    to='+91000000000'
+    to='+910000000000'
     )
 
     print(message.sid)
